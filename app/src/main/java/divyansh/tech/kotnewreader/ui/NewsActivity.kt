e32074e81eb09819f4f52e54133e8dac7042be47
justdvnsh <justdvnsh2208@gmail.com>
2020-06-17 14:28:49 +0530

    added search view in toolbar !!

--- app/src/main/java/divyansh/tech/kotnewreader/ui/NewsActivity.kt
+++ app/src/main/java/divyansh/tech/kotnewreader/ui/NewsActivity.kt
@@ -1,7 +1,19 @@
 package divyansh.tech.kotnewreader.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
+import androidx.core.view.MenuItemCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import divyansh.tech.kotnewreader.R
@@ -9,10 +21,71 @@ import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
 
+    private lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+
+    // set options -> Menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // casting the menu
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        // getting the search button & search view
+        var searchViewItem: MenuItem? = menu?.findItem(R.id.search)
+        searchView = MenuItemCompat.getActionView(searchViewItem) as SearchView
+
+        // setting on close listener
+        searchView.setOnCloseListener(object : SearchView.OnCloseListener {
+            override fun onClose(): Boolean {
+                return true
+            }
+        })
+
+        // setting the search plate to enter the query
+        val searchPlate = searchView.findViewById(androidx.appcompat.R.id.search_src_text) as EditText
+        searchPlate.hint = "Search"
+        val searchPlateView: View =
+            searchView.findViewById(androidx.appcompat.R.id.search_plate)
+        searchPlateView.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                android.R.color.transparent
+            )
+        )
+
+        // get the edit text to max width
+        searchView.maxWidth = Int.MAX_VALUE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // do your logic here
+//                 Toast.makeText(applicationContext, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                newsNavHostFragment.findNavController().navigate(R.id.searchFragment)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        val searchManager =
+            getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onBackPressed() {
+        if (!searchView.isIconified()) {
+            searchView.onActionViewCollapsed();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/divyansh/tech/kotnewreader/ui/fragments/SearchFragment.kt
@@ -0,0 +1,7 @@
+package divyansh.tech.kotnewreader.ui.fragments
+
+import androidx.fragment.app.Fragment
+import divyansh.tech.kotnewreader.R
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+}
