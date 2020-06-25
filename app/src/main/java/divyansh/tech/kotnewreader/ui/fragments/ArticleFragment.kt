0a89670d2811bdb4f51210e899e3d04cb5627eee
justdvnsh <justdvnsh2208@gmail.com>
2020-06-25 23:02:49 +0530

    added webview to read articles

--- app/src/main/java/divyansh/tech/kotnewreader/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/divyansh/tech/kotnewreader/ui/fragments/ArticleFragment.kt
@@ -1,6 +1,37 @@
 package divyansh.tech.kotnewreader.ui.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import divyansh.tech.kotnewreader.R
+import kotlinx.android.synthetic.main.common_toolbar.view.*
+import kotlinx.android.synthetic.main.fragment_article.*
 
-class ArticleFragment: Fragment(R.layout.fragment_article) {}
+class ArticleFragment: BaseFragment() {
+
+    val args: ArticleFragmentArgs by navArgs()
+
+    override fun provideView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.titleText.text = args.article.title.substring(0, 15) + "...."
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(args.article.url)
+        }
+    }
+}
--- app/src/main/java/divyansh/tech/kotnewreader/ui/fragments/FavouritesFragment.kt
+++ app/src/main/java/divyansh/tech/kotnewreader/ui/fragments/FavouritesFragment.kt
@@ -7,11 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import divyansh.tech.kotnewreader.R
+import divyansh.tech.kotnewreader.adapters.NewsAdapter
 import kotlinx.android.synthetic.main.common_toolbar.*
 import kotlinx.android.synthetic.main.common_toolbar.view.*
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import kotlinx.android.synthetic.main.fragment_favourites.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FavouritesFragment : BaseFragment() {
+
+    @Inject
+    lateinit var newsAdapter: NewsAdapter
 
     override fun provideView(
         inflater: LayoutInflater,
@@ -25,5 +36,22 @@ class FavouritesFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         view.titleText.text = "Favourites"
         Log.i("INJECTED FROM FAV ", viewModel.newRepository.db.hashCode().toString() + " api ->" + viewModel.newRepository.api.hashCode().toString())
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_favouritesFragment_to_articleFragment,
+                bundle
+            )
+        }
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
