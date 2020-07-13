20700c91faa0c871e43e107e9be3c3d82f7f4db9
justdvnsh <justdvnsh2208@gmail.com>
2020-07-14 00:32:36 +0530

    synthesized text to speech to audio files

--- app/src/main/java/divyansh/tech/kotnewreader/ui/AudioPlayerActivity.kt
+++ app/src/main/java/divyansh/tech/kotnewreader/ui/AudioPlayerActivity.kt
@@ -3,12 +3,9 @@ package divyansh.tech.kotnewreader.ui
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
-import android.widget.GridLayout
-import android.widget.GridView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -17,12 +14,16 @@ import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 import divyansh.tech.kotnewreader.R
 import divyansh.tech.kotnewreader.adapters.NewsAdapter
+import divyansh.tech.kotnewreader.network.models.Article
 import divyansh.tech.kotnewreader.ui.viewModels.newsViewModel
 import divyansh.tech.kotnewreader.utils.Resource
 import kotlinx.android.synthetic.main.activity_audio_player.*
 import java.io.File
-import java.io.PrintWriter
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 @AndroidEntryPoint
 class AudioPlayerActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
@@ -31,6 +32,7 @@ class AudioPlayerActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     lateinit var newsAdapter: NewsAdapter
     lateinit var tts: TextToSpeech
     val viewModel: newsViewModel by viewModels()
+    val utteranceId: String = "TechReads"
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,28 +49,32 @@ class AudioPlayerActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private fun setupListeners() {
         business.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.business))
+            loadArticlesAndDirectory(getString(R.string.business))
         }
         entertainment.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.entertainment))
+            loadArticlesAndDirectory(getString(R.string.entertainment))
         }
         technology.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.tech))
+            loadArticlesAndDirectory(getString(R.string.tech))
         }
         sports.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.sports))
+            loadArticlesAndDirectory(getString(R.string.sports))
         }
         health.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.health))
+            loadArticlesAndDirectory(getString(R.string.health))
         }
         science.setOnClickListener {
-            viewModel.getBreakingNews("in", getString(R.string.science))
+            loadArticlesAndDirectory(getString(R.string.science))
         }
         favorites.setOnClickListener {
             viewModel.getAllArticles().observe(this, Observer {
-                newsAdapter.differ.submitList(it)
+                saveAudio(it)
             })
         }
+    }
+
+    private fun loadArticlesAndDirectory(name: String) {
+        viewModel.getBreakingNews("in", name)
     }
 
     fun showProgress(progressBar: ProgressBar) {
@@ -79,26 +85,13 @@ class AudioPlayerActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         progressBar.visibility = View.GONE
     }
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun setupObservers() {
         viewModel.breakingNews.observe(this, Observer {
             when (it) {
                 is Resource.Success -> {
                     hideProgress(paginationProgressBar)
                     it.data?.let {
-                        var newses: MutableList<String?> = ArrayList<String?>()
-                        for (news in it.articles) {
-                            Log.i("Audio", news.title!!)
-                            newses.add(news.title + "  " + news.description)
-                        }
-                        Log.i("Audio", newses.toString())
-                        for (news in 0 until newses.size) {
-                            Log.i("Audio", baseContext.getExternalFilesDir(null).toString())
-                            val file = File(baseContext.getExternalFilesDir(null), "KotNews")
-                            if (!file.exists()) file.mkdir()
-                            val f = File(file, "news-${news}.txt")
-                            tts.synthesizeToFile(newses.get(news), null, f, null)
-                        }
+                        saveAudio(it.articles)
                     }
                 }
 
@@ -115,6 +108,33 @@ class AudioPlayerActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
         })
     }
+
+    private fun saveAudio(articles: List<Article>) {
+        var newses: MutableList<String?> = ArrayList<String?>()
+        for (news in articles) {
+            Log.i("Audio", news.title!!)
+            newses.add(news.title + "  " + news.description)
+        }
+        val directory = File(baseContext.getExternalFilesDir(null) , "/KotNews/${SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())}")
+        directory.delete()
+        directory.mkdir()
+        for (index in 0 until newses.size) {
+            Log.i("Audio", cacheDir.toString())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                tts.synthesizeToFile(
+                    newses.get(index),
+                    null,
+                    File(directory, utteranceId + "_news_${index}.mp3"),
+                    utteranceId
+                )
+            } else {
+                val params: HashMap<String, String> = HashMap()
+                params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, utteranceId);
+                tts.synthesizeToFile(newses.get(index), params, File(cacheDir, utteranceId + "_news-${index}.mp3").path)
+            }
+        }
+    }
+
 
     override fun onInit(status: Int) {
     }
